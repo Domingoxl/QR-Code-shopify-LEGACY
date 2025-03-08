20bb0e84d99d0dd5b51fde64c869a66d6b1d40f9
Domingoxl <damiengv123@gmail.com>
2025-03-07 21:55:15 -0500

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import os
-print("Template Directory:", os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates')))
-app = Flask(__name__, template_folder='server/templates')
+
+app = Flask(__name__, template_folder='server/templates')  # Update the path
+
 @app.route('/', methods=['GET', 'HEAD'])
 def home():
     return render_template('index.html')
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Ensure Flask uses the correct port
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
