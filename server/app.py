b189f6a5597250952a15f91f7d09c9d5185c208b
Domingoxl <damiengv123@gmail.com>
2025-03-08 12:18:21 -0500

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template
-import os
 
-# Ensure Flask looks for templates in the correct directory
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), "templates"))
+app = Flask(__name__, template_folder="server/templates")
 
-@app.route('/', methods=['GET', 'HEAD'])
+@app.route('/')
 def home():
-  return "<h1>QR Code Memorial App Running Successfully!</h1>"
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Ensure Flask uses the correct port
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
