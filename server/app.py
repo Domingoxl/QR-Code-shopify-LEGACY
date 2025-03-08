3a7d8388bc725afc77a2fb633bf4ad2aaec6a25e
Domingoxl <damiengv123@gmail.com>
2025-03-07 20:23:55 -0500

    Update app.py

--- server/app.py
+++ server/app.py
@@ -11,4 +11,4 @@ def products():
     return render_template('products.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
